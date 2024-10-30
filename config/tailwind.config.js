77cd6ed4e4465ca3c63259db1774ceb1aa04f828
zehan12 <zehan9211@gmail.com>
2024-10-30 21:09:46 +0530

    configure tailwind css

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,22 +1,13 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
+    content: [
+        "./public/*.html",
+        "./app/helpers/**/*.rb",
+        "./app/javascript/**/*.js",
+        "./app/views/**/*",
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    plugins: [],
+};
