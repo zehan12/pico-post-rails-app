bd56ce59a6f73584292c42515d494f2a13fa1727
zehan12 <zehan9211@gmail.com>
2024-10-30 17:10:08 +0530

    update home with index namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "static_pages/index"
+  get "static_pages/home"
   get "static_pages/help"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
